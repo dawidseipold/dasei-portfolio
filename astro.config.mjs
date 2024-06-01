3ebcc8816cc111b86aca171dd874395157844c17
dawidseipold <seipold.dawid@gmail.com>
2024-06-01 23:37:16 +0200

    build: add icon and react plugins to astro chore: remove unused Card component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
+    }),
+    icon(),
+    react({
+      experimentalReactChildren: true,
     }),
   ],
 });
