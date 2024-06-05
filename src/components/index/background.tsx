6ec676326040e7abed700735c140902ca4ee7a79
dawidseipold <seipold.dawid@gmail.com>
2024-06-05 11:53:30 +0200

    feat: add landing page background

---
+++ src/components/index/background.tsx
@@ -0,0 +1,12 @@
+import Spline from "@splinetool/react-spline";
+
+const Background = () => {
+  return (
+    <Spline
+      className="absolute top-0 left-0 z-0"
+      scene="https://prod.spline.design/k9Dp28P1yebzFvs2/scene.splinecode"
+    />
+  );
+};
+
+export default Background;
