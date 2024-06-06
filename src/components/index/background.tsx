81c3d2f18d49bb9df6f479f6a915534112b8c446
dawidseipold <seipold.dawid@gmail.com>
2024-06-06 15:04:21 +0200

    feat: update home page design and other aspects feat: add background component (3d spline on load, before image)

--- src/components/index/background.tsx
+++ src/components/index/background.tsx
@@ -1,11 +1,26 @@
 import Spline from "@splinetool/react-spline";
+import { useEffect, useState } from "react";
+import React from "react";
 
 const Background = () => {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <Spline
-      className="absolute top-0 left-0 z-0"
-      scene="https://prod.spline.design/k9Dp28P1yebzFvs2/scene.splinecode"
-    />
+    <>
+      <Spline
+        className="absolute top-0 left-0 z-10"
+        scene="https://prod.spline.design/k9Dp28P1yebzFvs2/scene.splinecode"
+        onLoad={() => setLoading(false)}
+      />
+
+      {loading && (
+        <img
+          className="absolute top-0 left-0 z-0 w-screen h-screen object-cover"
+          src="/background.png"
+          alt="background"
+        />
+      )}
+    </>
   );
 };
 
