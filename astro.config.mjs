66b5b70e81307bf1d35e4193cc7b9c97a3919b03
dawidseipold <seipold.dawid@gmail.com>
2024-06-12 20:49:57 +0200

    build: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,4 +17,5 @@ export default defineConfig({
     }),
   ],
   output: "hybrid",
+  adapter: vercel(),
 });
