5424c06720b8c389a52de69f06c8f003365f00f6
dawidseipold <seipold.dawid@gmail.com>
2024-06-12 20:42:53 +0200

    feat: change the way the send-message api is handled (temporary solution until fix is present) chore: remove .json from the name of the route

---
+++ src/pages/api/emails/send-message.ts
@@ -0,0 +1,41 @@
+import type { APIRoute } from "astro";
+import { Resend } from "resend";
+export const prerender = false;
+
+interface ContactFormValues {
+  topic: string;
+  name: string;
+  email: string;
+  message: string;
+}
+
+const normalizeTopic = (topic: string) => {
+  const words = topic.split("-");
+  return words.map((word) => word[0].toUpperCase() + word.slice(1)).join(" ");
+};
+
+export const POST: APIRoute = async ({ request }: { request: Request }) => {
+  // TEMPORARY SOLUTION UNTIL ASTRO FIXES DATA PARSING
+  let body = JSON.parse(
+    Object.fromEntries(new URL(await request.url).searchParams).data
+  ) as ContactFormValues;
+
+  const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+  const { error } = await resend.emails.send({
+    from: "Dawid Seipold <no-reply@dawidseipold.com>",
+    to: [body.email],
+    subject: `Dawid Seipold - ${normalizeTopic(body.topic)}`,
+    html: `
+      <h1>Hi, ${body.name}!</h1>
+      <p>Thanks for contacting me! I'll get back to you as soon as possible.</p>
+      <p>Greetings, <b>Dawid Seipold</b>!</p>
+    `,
+  });
+
+  if (error) {
+    return new Response(error.message, { status: 500 });
+  }
+
+  return new Response("Message sent successfully", { status: 200 });
+};
